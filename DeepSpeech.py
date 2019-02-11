d8f137ed165d2e30c55234fd4984397be0dbb8d5
akmal13 <akmalnajid@gmail.com>
2019-02-11 18:07:30 +0700

    Seventh

--- DeepSpeech.py
+++ DeepSpeech.py
@@ -881,36 +881,24 @@ def do_single_file_inference(input_file_path):
                         Config.alphabet)
         decoded = ctc_beam_search_decoder(logits, Config.alphabet, FLAGS.beam_width, scorer=scorer)
         # Print highest probability result
-        karakter = len(logits[0])
-        panjang = len(logits)
+        karakter = len(logits[0]) # Banyak Karakter
+        panjang = len(logits)     # Panjang dari logits
         #print('Karakter : '+ str(karakter))
         #print('Panjang Sound : '+ str(panjang))
+        max_proba = [0 for _ in range(panjang)] # Max Probability
+        max_prob = [[0]*3 for _ in range(panjang)] # Max Probability untuk 3
+        kartek = [0 for _ in range(panjang)]  # Karakter tertinggi
+        makartek = [[0]*3 for _ in range (panjang)] # 3 Karakter tertinggi
 
-
-        ### print(decoded[1023][1])
-        stringhas = decoded[0][1]
-        print(stringhas)
-        stringtran = [99 for _ in range((len(stringhas)))]
-        alpabet = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-        for c in range (0,len(stringhas)):                                                                                                  # Merubah huruf menjadi angka
-            for v in range (0,len(alpabet)):
-                if(stringhas[c] == alpabet[v]):
-                    stringtran[c] = v
-
-        print(stringtran)
-
-
-        max_proba = [0 for _ in range(panjang)]
-        max_prob = [[0]*3 for _ in range(panjang)] # Buat 3
-        kartek = [0 for _ in range(panjang)]
-        makartek = [[0]*3 for _ in range (panjang)]
-
-        for x in range (0,panjang):
-            for y in range (0,karakter):
+        for x in range (0,panjang-1):
+            for y in range (0,karakter-1):
+                ##print('nice')
                 if logits[x][y] > max_proba[x] :
                     max_proba[x] = logits[x][y]
                     kartek[x] = y
-        print (kartek)
+        
+        # Max 3 Harus di rebuild rebuild
+
         for x in range (0,panjang):
             for z in range (0,3):
                 for y in range (0,karakter):
@@ -918,42 +906,29 @@ def do_single_file_inference(input_file_path):
                         max_prob[x][z] = logits[x][y]           # Buat 3
                         makartek[x][z] = y
                         logits[x][y] = 0
-    
+        
         #print(makartek)
-        mangsebelum = 0
-        sebelum = 0
-        ketemu = 0
-        print('Panjang-1 : '+ str(panjang-1))
-        for i in range (len(stringtran)):
-            ketemu = 0
-            for j in range (0,panjang):
-                if stringtran[i] == makartek[j][0]:
-                    ketemu = 1
-                    sebelum = j
-                    mangsebelum = j
-                    print('J : '+str(j) + 'Dengan huruf '+str(stringtran[i]))
-                    #break
+        #print(kartek)   
 
-                if ketemu == 0 and j == panjang-1:
-                    for n in range (len(makartek[0])):
-                        for k in range (sebelum,panjang-1):
-                            print ('PAPAPPAPAPA')
-                            if makartek[k][n] == stringtran[i]:
-                                makartek[k][0] == stringtran[i]
-                    #ketemu = 1
-                    print('LALALLALALALAL')
-                    
+        ### print(decoded[1023][1])
+        stringhas = decoded[0][1]   # String tertinggi
+        print(stringhas)
+        stringtran = [99 for _ in range((len(stringhas)))]  # Menyimpan angka dari string
+        alpabet = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for c in range (0,len(stringhas)):
+            for v in range (0,len(alpabet)):
+                if(stringhas[c] == alpabet[v]):
+                    stringtran[c] = v
 
-        print(makartek)                            
+        print('Stringtran : '+str(stringtran))
 
-
-        penanda = kartek[0]
+        penanda = kartek[0]  # penanda awal dari karakter
         kartekmod = [kartek for _ in range(0,len(kartek))]
         #print(len(kartek))
         #print(kartek[123])
-        nostop = 0
-        itan = 0
-        nanta = 0
+        nostop = 0   # Batas apakah akan berhenti
+        itan = 0     # Iterasi dari karakter
+        nanta = 0    # Banyak dari karakter
         kartek.append(99)
         
         while nostop != 1:
@@ -989,7 +964,7 @@ def do_single_file_inference(input_file_path):
                 manana = math.floor(len(kartekmod[i])/2)
                 kartekmod[i][manana] = nilai
         #print(kartekmod)
-        pembanding = [0]
+        pembanding = [0]  # Pembanding untuk Waktu
         
 
         for i in range (0,nanta+1):
@@ -1000,23 +975,23 @@ def do_single_file_inference(input_file_path):
         print('Panjang Dari Suara : ' + str(len(pembanding)))
         #print(kartekmod)
         #print(pembanding)
-        lenstring = len(stringtran)
-        muncul = [0 for _ in range (lenstring)]
-        counter = 0
+        lenstring = len(stringtran)   # Panjang dari string
+        muncul = [0 for _ in range (lenstring)]   # Kemunculan dari Karakter
+        counter = 0  # Counter untuk loop
         #print(str(len(stringtran)))
         #print(str(len(pembanding)))
         
-        mamama = len(pembanding)
+        pbanding = len(pembanding) # panjang dari pembanding
         loop = 0
         for i in range (0,len(stringtran)):
-            while loop < mamama:
+            while loop < pbanding:
                 if(stringtran[i] == pembanding[loop]):
                    if i != 0 :  
                         muncul[i] = loop+muncul[i-1]
                    else :
                        muncul[i] = loop
                    del pembanding [:loop]
-                   mamama = len(pembanding)
+                   pbanding = len(pembanding)
                    loop = 0
                    break
                 counter = counter + 1
@@ -1025,18 +1000,15 @@ def do_single_file_inference(input_file_path):
         #print(str(len(stringtran)))
         #print(muncul)
 
-        f = sf.SoundFile(input_file_path)
-        waktu = format(len(f) / f.samplerate)
+        f = sf.SoundFile(input_file_path)   # file yang digunakan
+        waktu = format(len(f) / f.samplerate) # Menghitung waktu dari file
         waktu = float(waktu)
         print('seconds = ' + str(waktu))
 
         print('')
         print('')
 
-        maxi = float(muncul[0])
-
-        mankan = [0 for _ in range (len(stringtran))]
-        tambura = 0
+        maxi = float(muncul[0])    # Maximum untuk menentukan waktu dari karakter
 
         for i in range (len(stringtran)):
             if(stringhas[i] != ' '):
@@ -1045,62 +1017,44 @@ def do_single_file_inference(input_file_path):
                     maxi = nilaii
                 nilak = nilaii * waktu / panjang
                 print('Waktu karakter '+str(i+1)+' dengan karakter '+str(stringhas[i])+' adalah : '+ str(nilak))
-                if nilak == 0:
-                            mankan[tambura] = stringhas[i]
-                            tambura = tambura + 1
             else:
                 print('')
             
-        
-        for i in range (len(alpabet)):
-            for m in range (len(mankan)):
-                if mankan[m] == alpabet[i]:
-                    mankan[m] = i
 
-        while 0 in mankan: mankan.remove(0)
-
-        print(str(mankan))
-        print(stringhas)
         print(' ')
 
-        kata = 0
-        katama = []
-        pampam = 1
+        kata = 0     
+        katama = []  # kata yang telah masuk
+        penkat = 1   # penanda kata
 
-        panpama = [0 for _ in range (len(stringhas))]
+        wdtan = [0 for _ in range (len(stringhas))]
         for i in range (len(stringhas)):
-            panpama[i] = stringhas[i]
+            wdtan[i] = stringhas[i]
         
-        panpama.append(' ')
+        wdtan.append(' ')
         sebelum = 0
         str1 = ''
-        for i in range (len(panpama)):
-            if panpama[i] == ' ':
+        for i in range (len(wdtan)):
+            if wdtan[i] == ' ':
                 nilaii = float(muncul[i-1])
                 while nilaii == 0:
-                    nilaii = float(muncul[i-1-pampam])
-                    pampam = pampam +1
-                pampam = 1
+                    nilaii = float(muncul[i-1-penkat])
+                    penkat = penkat +1
+                penkat = 1
                 nilam = float(muncul[sebelum])
                 nilak = ((nilaii+nilam)/2) * waktu / panjang
                 for j in range (sebelum,i):
-                    katama.append(panpama[j])
+                    katama.append(wdtan[j])
                 str1 = ''.join(katama)
                 if (i - sebelum) > 1:
                     #Kata lebih dari satu huruf
                     kata = kata + 1
                     print('\nWaktu kata '+str(kata)+' dengan kata " '+str1+' " dengan Waktu:\n\nAwal   : '+str(nilam*waktu/panjang)+'\nAkhir  : '+str(nilaii*waktu/panjang)+'\nTengah : '+str(nilak)+'\n')
-                        
-
                 katama = []
                 str1 = ''
                 sebelum = i+1
 
-    #Tadinya get 1 sekarang 3 terbesar
-    #setelah itu baru yang paling besar gak papa diubah ke yang 99 ,setelah itu suruh cari 
-    #kalo dia 99 cari yang yang sisanya kalo contoh jakarta jadinya rta 
-    #dan pada pencarian setelah placement dari sebelumnya dan sebelum yang setelahnya, ketemu sekali 
-    #kalo kayak a yang muncul beberapa kali gak papa,cari aja semuanya baik a pertama kedua dan ketiga  
+    # Untuk pada kata yang pada kemungkinan lebih masih terdapat kesalahan        
 
 
 
